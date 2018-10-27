2b95e607b4a5b7c947fe9b18899be043f4adcc11
Taylor <minnietj@hotmail.com>
2018-10-27 10:11:52 -0700

    Added (german) sample code for using MSP430 for MCP2515 :)

---
+++ Using MSP430 with MCP2515 Sample Code/iar-mcp2515-beispielcode/main.c
@@ -0,0 +1,65 @@
+// ****************************** PIN-OUT **************************************
+//
+// MSP430G2553 - Funktion - MCP2515
+// ---------------------------------  
+//    P1.4     -   !INT   -  Pin 12
+//    P1.5     -   SCK    -  Pin 13
+//    P1.6     -   MOSI   -  Pin 14
+//    P1.7     -   MISO   -  Pin 15
+//    P2.5     -   !CS    -  Pin 16
+//
+// *****************************************************************************
+
+#include "mcp2515.h"
+
+
+can_t can_tx;                                                                    // CAN-Sendevariable
+can_t can_rx;                                                                    // CAN-Empfangsvariable
+  
+int main(void)
+{  
+  // --- 1. MSP430-Stop Watchdog -----------------------------------------------
+  
+  WDTCTL = WDTPW + WDTHOLD;                                                      // Stop Watchdog-Timer 
+
+  // --- 2. Setze MSP430-Clock -------------------------------------------------
+  
+  if (CALBC1_1MHZ==0xFF) while(1);  					         // If calibration constant erased. Do not load, trap CPU!!	  
+  DCOCTL = 0;                                                                    // Nehme niedrigste Frequenz, DCOx and MODx settings
+  BCSCTL1 = CALBC1_1MHZ;                                                         // Range
+  DCOCTL = CALBC1_1MHZ;                                                          // DCO-Step + Modulation 
+
+  // --- 3. MSP430-Pin-IR und LED Einstellen -----------------------------------
+  
+  P1DIR |= BIT0;                                                                 // P1.0 als Ausgang (LED am EXP430G2)                                                            
+  P1OUT |=  BIT4;                                                                // P1.4 setzen
+  P1REN |= BIT4;                                                                 // P1.4 Pullup
+  P1IE |= BIT4;                                                                  // P1.4 Interrupt aktivieren (nur für diesen Pin)
+  P1IES |= BIT4;                                                                 // P1.4 Hi/lo Edge
+  P1IFG &= ~BIT4;                                                                // P1.4 IFG Cleared 
+  
+  // --- 4. Init SPI, MMCP2515 und CAN-Variablen -------------------------------
+  
+  MCP2515_SPI_init();                                                            // Initialisiert SPI
+  MCP2515_init();                                                                // Initialisiert MCP2515
+  MCP2515_CanVariable_init (&can_tx);                                            // Initialisiert Sendevariable
+  MCP2515_CanVariable_init (&can_rx);                                            // Initialisiert Empfangsvariable
+  
+  _EINT();                                                                       // Aktiviere Interrupts sonst ist der MSP430-Pin-IR nutzlos
+  
+  while(1)                                                                       // Endlosschleife
+  { 
+    P1OUT ^= BIT0;                                                               // P1.0 Togglen
+    __delay_cycles(DELAY_1s);                                                    // Warte 1 Sekunden
+  } // while
+}
+    
+// Port 1: Interrupt-Service-Routine
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1(void)
+{
+  MCP2515_can_rx0(&can_rx);                                                      // Lese Information auS Empfangskanal RX0
+  __delay_cycles(DELAY_10ms);                                                    // Warte 10ms
+  MCP2515_can_tx0(&can_rx);                                                      // Sende das Empfangene zurück (Echo)
+  P1IFG &= ~BIT4;                                                                // P1.4 IFG wieder öschen
+}
